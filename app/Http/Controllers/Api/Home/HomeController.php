fbb6883d0256b89cd06269c510d5fbc2ce2ed0f4
kj6560 <kj6560@gmail.com>
2025-03-13 06:05:01 +0530

    a

--- app/Http/Controllers/Api/Home/HomeController.php
+++ app/Http/Controllers/Api/Home/HomeController.php
@@ -7,20 +7,27 @@ use App\Models\Inventory;
 use App\Models\Orders;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function fetchKpi()
     {
+        if (!$this->checkSubscription(Auth::user()->id)) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'You do not have an active subscription'
+            ], 403);
+        }
         return response()->json([
             'status' => 'success',
             'data' => [
                 "sales" => [
-                    'sales_today' => Orders::select(DB::raw("sum(orders.net_total) as sales_today"))->where('created_at', '>=', date('Y-m-d 00:00:00'))->first()->sales_today??"NA",
-                    'sales_this_month' => Orders::select(DB::raw("sum(orders.net_total) as sales_this_month"))->where('created_at', '>=', date('Y-m-01 00:00:00'))->first()->sales_this_month??"NA",
-                    'sales_total' => Orders::select(DB::raw("sum(orders.net_total) as sales_total"))->first()->sales_total??"NA"
-                    
+                    'sales_today' => Orders::select(DB::raw("sum(orders.net_total) as sales_today"))->where('created_at', '>=', date('Y-m-d 00:00:00'))->first()->sales_today ?? "NA",
+                    'sales_this_month' => Orders::select(DB::raw("sum(orders.net_total) as sales_this_month"))->where('created_at', '>=', date('Y-m-01 00:00:00'))->first()->sales_this_month ?? "NA",
+                    'sales_total' => Orders::select(DB::raw("sum(orders.net_total) as sales_total"))->first()->sales_total ?? "NA"
+
                 ],
                 "inventory" => [
                     'inventory_added_today' => Inventory::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\UserFeaturePurchase;
+
 abstract class Controller
 {
-    //
+    public function checkSubscription($userId)
+    {
+        $hasActiveSubscription = UserFeaturePurchase::where('user_id', $userId)
+            ->where(function ($query) {
+                $query->whereNull('expires_at') // Lifetime purchase
+                    ->orWhere('expires_at', '>', now()); // Active subscription
+            })
+            ->exists();
+        return $hasActiveSubscription;
+    }
 }
