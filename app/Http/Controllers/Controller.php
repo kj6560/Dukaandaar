2795d6cabc879290380fd6a1dc42faaf1589ecfc
kj6560 <kj6560@gmail.com>
2025-03-13 07:19:08 +0530

    a

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,11 +11,11 @@ abstract class Controller
         $hasActiveSubscription = UserFeaturePurchase::where('user_id', $userId)
             ->where(function ($query) {
                 $query->whereNull('expires_at') // Lifetime purchase
-                    ->orWhere('expires_at', '>', now())
-                    ->orWhere('expired', 1)
-                    ; // Active subscription
+                    ->orWhere('expires_at', '>', now()); // Active subscription
             })
+            ->where('expired', 0) // Fix: Use `where()` instead of `andWhere()`
             ->exists();
+
         return $hasActiveSubscription;
     }
 }
