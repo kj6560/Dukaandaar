8ea74ae632c408d91268925ab4f266561028fe7a
kj6560 <kj6560@gmail.com>
2025-03-13 06:35:50 +0530

    a

--- app/Http/Controllers/Api/Home/HomeController.php
+++ app/Http/Controllers/Api/Home/HomeController.php
@@ -17,24 +17,24 @@ class HomeController extends Controller
         if (!$this->checkSubscription(Auth::user()->id)) {
             return response()->json([
                 'status' => 'error',
-                'message' => 'You do not have an active subscription'
+                'data' => []
             ], 403);
         }
         return response()->json([
             'status' => 'success',
             'data' => [
-                "sales" => [
+                "sales_data" => [
                     'sales_today' => Orders::select(DB::raw("sum(orders.net_total) as sales_today"))->where('created_at', '>=', date('Y-m-d 00:00:00'))->first()->sales_today ?? "NA",
                     'sales_this_month' => Orders::select(DB::raw("sum(orders.net_total) as sales_this_month"))->where('created_at', '>=', date('Y-m-01 00:00:00'))->first()->sales_this_month ?? "NA",
                     'sales_total' => Orders::select(DB::raw("sum(orders.net_total) as sales_total"))->first()->sales_total ?? "NA"
 
                 ],
-                "inventory" => [
+                "inventory_data" => [
                     'inventory_added_today' => Inventory::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
                     'inventory_added_this_month' => Inventory::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
                     'inventory_added_total' => Inventory::count()
                 ],
-                "products" => [
+                "products_data" => [
 
                     'products_added_today' => Product::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
                     'products_added_this_month' => Product::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
