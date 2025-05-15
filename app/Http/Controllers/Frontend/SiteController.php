8a90a66cd92ded522705d86642e17fbb67baf944
kj6560 <kj6560@gmail.com>
2025-05-15 18:25:34 +0530

    a

--- app/Http/Controllers/Frontend/SiteController.php
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SiteController extends Controller
 {
