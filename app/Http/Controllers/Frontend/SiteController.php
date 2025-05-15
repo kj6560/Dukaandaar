809a5375204db15f1c4220eb429748c459b506db
kj6560 <kj6560@gmail.com>
2025-05-15 18:24:19 +0530

    a

--- app/Http/Controllers/Frontend/SiteController.php
+++ app/Http/Controllers/Frontend/SiteController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Container\Attributes\Auth;
 use Illuminate\Http\Request;
 
 class SiteController extends Controller
 {
     public function index(Request $request){
+        if(Auth::check()){
+            return redirect("/admin/dashboard");
+        }
         return view("frontend.index");
     }
 }
