fe5b383bc27a346bc720771d1b341c4f2f98245a
kj6560 <kj6560@gmail.com>
2025-05-19 12:25:23 +0530

    a

--- app/Http/Controllers/Api/Home/HomeController.php
+++ app/Http/Controllers/Api/Home/HomeController.php
@@ -12,9 +12,9 @@ use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
-    public function fetchKpi()
+    public function fetchKpi(Request $request)
     {
-        if (!$this->checkSubscription(Auth::user()->id)) {
+        if (!$this->checkSubscription(Auth::user()->id) || empty($request->device_id)) {
             return response()->json([
                 'status' => 'error',
                 'data' => [
@@ -39,28 +39,56 @@ class HomeController extends Controller
                 ]
             ], 200);
         }
-        return response()->json([
-            'status' => 'success',
-            'data' => [
-                "sales_data" => [
-                    'sales_today' => Orders::select(DB::raw("sum(orders.net_total) as sales_today"))->where('created_at', '>=', date('Y-m-d 00:00:00'))->first()->sales_today ?? "NA",
-                    'sales_this_month' => Orders::select(DB::raw("sum(orders.net_total) as sales_this_month"))->where('created_at', '>=', date('Y-m-01 00:00:00'))->first()->sales_this_month ?? "NA",
-                    'sales_total' => Orders::select(DB::raw("sum(orders.net_total) as sales_total"))->first()->sales_total ?? "NA"
+        $user = Auth::user();
 
-                ],
-                "inventory_data" => [
-                    'inventory_added_today' => Inventory::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
-                    'inventory_added_this_month' => Inventory::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
-                    'inventory_added_total' => Inventory::count()
-                ],
-                "products_data" => [
+        if ($user->device_id == $request->device_id) {
+            return response()->json([
+                'status' => 'success',
+                'data' => [
+                    "sales_data" => [
+                        'sales_today' => Orders::select(DB::raw("sum(orders.net_total) as sales_today"))->where('created_at', '>=', date('Y-m-d 00:00:00'))->first()->sales_today ?? "NA",
+                        'sales_this_month' => Orders::select(DB::raw("sum(orders.net_total) as sales_this_month"))->where('created_at', '>=', date('Y-m-01 00:00:00'))->first()->sales_this_month ?? "NA",
+                        'sales_total' => Orders::select(DB::raw("sum(orders.net_total) as sales_total"))->first()->sales_total ?? "NA"
 
-                    'products_added_today' => Product::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
-                    'products_added_this_month' => Product::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
-                    'products_added_total' => Product::count()
+                    ],
+                    "inventory_data" => [
+                        'inventory_added_today' => Inventory::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
+                        'inventory_added_this_month' => Inventory::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
+                        'inventory_added_total' => Inventory::count()
+                    ],
+                    "products_data" => [
 
+                        'products_added_today' => Product::where('created_at', '>=', date('Y-m-d 00:00:00'))->count(),
+                        'products_added_this_month' => Product::where('created_at', '>=', date('Y-m-01 00:00:00'))->count(),
+                        'products_added_total' => Product::count()
+
+                    ]
                 ]
-            ]
-        ]);
+            ]);
+        }else{
+            return response()->json([
+                'status' => 'error',
+                'data' => [
+                    "sales_data" => [
+                        'sales_today' => "NA",
+                        'sales_this_month' => "NA",
+                        'sales_total' => "NA"
+
+                    ],
+                    "inventory_data" => [
+                        'inventory_added_today' => "NA",
+                        'inventory_added_this_month' => "NA",
+                        'inventory_added_total' => "NA"
+                    ],
+                    "products_data" => [
+
+                        'products_added_today' => "NA",
+                        'products_added_this_month' => "NA",
+                        'products_added_total' => "NA"
+
+                    ]
+                ]
+            ], 401);
+        }
     }
 }
