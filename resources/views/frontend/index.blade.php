4f010d5499d0c514a700752fa0447083e5e4cec3
kj6560 <kj6560@gmail.com>
2025-05-19 13:50:28 +0530

    a

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -91,7 +91,7 @@
     <!-- Hero Section -->
     <section id="hero" class="hero section dark-background">
 
-      <img src="assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
+      <img src="{{ asset('theme') }}/assets/img/hero-bg.jpg" alt="" data-aos="fade-in">
 
       <div class="container" data-aos="fade-up" data-aos-delay="100">
         <div class="row justify-content-start">
