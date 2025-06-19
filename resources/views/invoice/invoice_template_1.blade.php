7141e4d194b651eeebd0ba3af2fbf096666a788a
kj6560 <kj6560@gmail.com>
2025-06-19 19:21:42 +0530

    minor change

---
+++ resources/views/invoice/invoice_template_1.blade.php
@@ -0,0 +1,24 @@
+       *** INVOICE ***       
+----------------------------
+Order ID   : {{ $order->order_id }}
+Date       : {{ \Carbon\Carbon::parse($order->order_date)->format('d M Y H:i') }}
+----------------------------
+Org        : {{ $organization->org_name ?? 'Customer' }}
+Customer   : {{ $order->customer_name ?? 'Customer' }}
+----------------------------
+@foreach ($orderDetails as $item)
+{{ Str::limit($item['product_name'], 20) }} x{{ $item['quantity'] }}
+Price      : ₹{{ number_format($item['base_price'], 2) }}
+Discount   : ₹{{ number_format($item['discount'], 2) }}
+Tax        : ₹{{ number_format($item['tax'], 2) }}
+Net        : ₹{{ number_format($item['net_price'], 2) }}
+----------------------------
+@endforeach
+Order Total: ₹{{ number_format($order->total_order_value, 2) }}
+Discount   : ₹{{ number_format($order->total_order_discount, 2) }}
+Tax        : ₹{{ number_format($order->tax, 2) }}
+Net Total  : ₹{{ number_format($order->net_total, 2) }}
+----------------------------
+Thank you for your purchase!
+----------------------------
+
